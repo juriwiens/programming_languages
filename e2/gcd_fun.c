80bc58fe1743eced3a4cde208c83f8fec16b665f
Juri Wiens <juriwiens@gmail.com>
2015-10-12 11:44:32 +0200

    Added unmodified sources of excercise 2

---
+++ e2/gcd_fun.c
@@ -0,0 +1,13 @@
+#include "gcd_fun.h"
+
+int gcd(int a, int b) {
+    while (a != b) {
+      if (a > b) {
+        a = a - b;
+      }
+      else {
+        b = b - a;
+      }
+    }
+    return a;
+}
---
+++ e2/gcd_main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "gcd_fun.h"
+
+int main()
+{
+    int i, j;
+    scanf("%d %d", &i, &j);
+    printf("%d\n", gcd(i, j));
+}
